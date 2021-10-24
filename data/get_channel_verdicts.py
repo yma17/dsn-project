61356e5ca81874a150873f7acdc88c190b7b68f9
Yingchen Ma <yingchem@umich.edu>
2021-10-24 14:57:40 -0400

    make get_channel_verdicts script more robust

--- data/get_channel_verdicts.py
+++ data/get_channel_verdicts.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import pandas as pd
+import os
 
 DRIVER_PATH = "/usr/local/bin/chromedriver"
 
@@ -31,24 +32,35 @@ def get_channel_verdict(channel_id):
     driver.close()
     return v
 
-def get_channel_verdicts(channel_lst):
+def get_channel_verdicts(channel_lst, data_old, save_interval=20):
     verdict_lst = []
     for i, channel_id in enumerate(channel_lst):
-        if i % 20 == 0:
+        if i % save_interval == 0:
             # Print summary of verdicts so far
             stats_str = "Summary at channel {}/{}: ".format(i, len(channel_lst))
             for v in VERDICTS:
                 stats_str += "{}: {}, ".format(v, verdict_lst.count(v))
             print(stats_str[:-2])
             # Save verdicts so far to file (in case any)
-            df = pd.DataFrame({"channel_id": channel_lst[:i], "verdict": verdict_lst})
+            df = pd.DataFrame({
+                "channel_id": data_old["channel_id"] + channel_lst[:i],
+                "verdict": data_old["verdict"] + verdict_lst})
             df.to_csv(CHANNEL_VERDICTS_CSV)
         verdict_lst += [get_channel_verdict(channel_id)]
     return verdict_lst
 
 if __name__ == '__main__':
-    channel_lst = list(pd.read_csv(CHANNEL_ID_CSV)["channel_ids"])
-    #channel_lst = channel_lst[:5]
-    verdict_lst = get_channel_verdicts(channel_lst)
-    df = pd.DataFrame({"channel_id": channel_lst, "verdict": verdict_lst})
+    channel_lst = pd.read_csv(CHANNEL_ID_CSV)["channel_ids"].tolist()  # should preserve order
+
+    if os.path.isfile(CHANNEL_VERDICTS_CSV):
+        data_old = pd.read_csv(CHANNEL_VERDICTS_CSV).reset_index().to_dict(orient='list')
+    else:
+        data_old = {"channel_id": [], "verdict": []}
+    
+    start_idx = 3  # can change
+
+    verdict_lst = get_channel_verdicts(channel_lst[start_idx:], data_old)
+    df = pd.DataFrame(
+        {"channel_id": data_old["channel_id"] + channel_lst[start_idx:],
+        "verdict": data_old["verdict"] + verdict_lst})
     df.to_csv(CHANNEL_VERDICTS_CSV)
