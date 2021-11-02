5eb06bd82a887afd41949597067cc6aebae3b291
Monish <monish.ramadoss@outlook.com>
2021-11-01 21:09:41 -0400

    trainable model

--- model/data_processor.py
+++ model/data_processor.py
@@ -1,10 +1,12 @@
+import os
 import pandas as pd
 import numpy as np
+from tqdm import tqdm
 from sentence_transformers import SentenceTransformer
 
 import re
 from emoji import demojize
-
+join = os.path.join
 
 model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2", device='cuda')
 
@@ -31,49 +33,52 @@ def chunk_txt(txt):
     chunks = [' '.join(lst[x: x+384]) for x in range(0, len(lst), 384)]
     return chunks
 
+
 if __name__ == '__main__':
-    comment_df = pd.read_csv('./tmp/baseline1_comments.csv', sep='\t')
-    video_df = pd.read_csv('./tmp/baseline1_videos.csv', sep='\t')[['video_id', 'title', 'captions']].to_numpy()
-    comment_embeddings = {'video_id': [], 'idx': []}
-    caption_embeddings = {'video_id': [], 'idx': []}
+    baseline = 'baseline1'
+    if not os.path.isdir(f'./{baseline}'):
+        os.makedirs(f'./{baseline}', exist_ok=True)
+
+    label_id = 'normalized_annotation' if baseline == 'baseline1' else 'misinformation'        
+    
+    comment_df = pd.read_csv(f'./tmp/{baseline}_comments.csv', sep='\t')
+    video_df = pd.read_csv(f'./tmp/{baseline}_videos.csv', sep='\t')[['video_id', 'title', 'captions', label_id]].to_numpy()
+    pbar = tqdm(total=video_df.shape[0])
+    _embedding = {'video_id': [], 'cap_idx': [], 'com_end_idx':[], 'com_start_idx':[], 'label':[], }
     comment_idx = 0
     caption_idx = 0
     
     embeddings = {"captions":[], "titles":[], "comments":[]}
     
-    for video_id, video_title, captions in video_df:
+    for video_id, video_title, captions, label in video_df:
         caption_embedd = convert_to_tokens(captions.strip('>'))
         title_embedd = convert_to_tokens(clean_txt(video_title), False)
 
         embeddings['captions'].append(caption_embedd)
         embeddings['titles'].append(title_embedd)
 
-        caption_embeddings['idx'].append(caption_idx)
-        caption_embeddings['video_id'].append(video_id)
-        caption_idx += 1
-
+        _embedding['cap_idx'].append(caption_idx)
+        _embedding['video_id'].append(video_id)
+        _embedding['label'].append(int(label))
+        _embedding['com_start_idx'].append(comment_idx)
         comment_seq = comment_df.loc[comment_df['video_id'] == video_id]['comment']
         for comment in comment_seq:
-            try:
+            try:                
                 comment_embedd = convert_to_tokens(clean_txt(str(comment)), False)
-                embeddings['comments'].append(comment_embedd)
-                comment_embeddings['video_id'].append(video_id)
-                comment_embeddings['idx'].append(comment_idx)
                 comment_idx += 1
+                embeddings['comments'].append(comment_embedd)
             except Exception as e:
                 print(e, clean_txt(comment))
-                
-       
-    comment_embedd_df = pd.DataFrame(comment_embeddings)
-    caption_embedd_df = pd.DataFrame(caption_embeddings)
-    comment_embedd_df.to_csv('./comment_embedd_1.csv')
-    caption_embedd_df.to_csv('./caption_embedd_1.csv')
 
-    captions = np.array(embeddings['captions'], dtype=object)
-    titles = np.array(embeddings['titles'], dtype=object)
-    comments = np.array(embeddings['comments'], dtype=object)
+        _embedding['com_end_idx'].append(comment_idx)
+        caption_idx += 1
+        pbar.update(1)
+    pbar.close()
+        
+    pd.DataFrame(_embedding).to_csv(f'./{baseline}/_embedd.csv', sep='\t')
 
-    np.save("caption_embedd_1.npy", captions)
-    np.save("title_embedd_1.npy", titles)
-    np.save("comments_embedd_1.npy", comments)
+    np.save(f"./{baseline}/caption_embedd.npy", np.array(embeddings['captions'], dtype=object))
+    np.save(f"./{baseline}/title_embedd.npy", np.array(embeddings['titles'], dtype=object))
+    np.save(f"./{baseline}/comments_embedd.npy", np.array(embeddings['comments'], dtype=object))
+    print(len(embeddings['captions']), len(embeddings['titles'], embedding['comments']))
 
